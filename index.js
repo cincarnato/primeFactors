a3c54016ff829e2cfeedba000c29ee0b1ca5b468
Cristian Incarnato <cristian.cdi@gmail.com>
2021-02-25 23:12:33 -0300

    Solved

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const primeFactors = function(num){
 
     /* YOUR CODE HERE */
+    let result = []
+
+    function factor(n){
+        for(let i=2; i<=n; i++){
+            if(n % i === 0){
+                result.push(i)
+                return n / i
+            }
+        }
+    }
+
+    while(num != 1){
+        num = factor(num)
+    }
+
+    return result.join(" ")
+
 
 }
 
